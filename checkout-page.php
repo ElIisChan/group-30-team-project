4b26d18bd689ee85afcc425c1be88e839f094998
Ellis Crowther <ellis.crowther@gambitnash.co.uk>
2022-12-01 11:26:57 +0000

    feature(theme): initial test

--- checkout-page.php
+++ checkout-page.php
@@ -3,46 +3,51 @@
         <?php session_start(); ?>
         <h1 style="color: #000;">Checkout</h1>
         <?php
-        $totalItemPrice = [];
-        foreach($_SESSION['cart'] as $key => $value) {
-            $totalItemPrice[] .= $value['product_price'] * $value['product_quantity'];
-        }
-        foreach($totalItemPrice as $price) {
-            $totalPrice += $price;
-        }
-        echo '<p style="color: #000;">To pay: £' . $totalPrice . '</p>';
-        ?>
-        <form action="./checkout-function.php" method="post">
-            <div class="checkout-container">
-                <p class="checkout-container-text">Full Name: </p>
-                <input type="text" name="username" class="checkout-container-input" required>
-            </div>
-            <div class="checkout-container">
-                <p class="checkout-container-text">Email Address: </p>
-                <input type="email" name="email" class="checkout-container-input" required>
-            </div>
-            <div class="checkout-container">
-                <p class="checkout-container-text">Address: </p>
-                <input type="text" name="address" class="checkout-container-input" required>
-            </div>
-            <div class="checkout-container">
-                <p class="checkout-container-text">Postcode: </p>
-                <input type="text" name="postcode" class="checkout-container-input" required>
-            </div>
-            <div class="checkout-container">
-                <p class="checkout-container-text">City: </p>
-                <input type="text" name="city" class="checkout-container-input" required>
-            </div>
-            <div class="checkout-container">
-                <p class="checkout-container-text">Country: </p>
-                <input type="text" name="country" class="checkout-container-input" required>
-            </div>
-            <div class="checkout-container">
-                <p class="checkout-container-text">Phone Number: </p>
-                <input type="text" name="phone" class="checkout-container-input" required>
-            </div>
-            <button id="checkout_button">Checkout</button>
-        </form>
+		if(isset($_SESSION['uservalid']) && $_SESSION['uservalid'] === "yes") {
+            $totalItemPrice = [];
+            foreach($_SESSION['cart'] as $key => $value) {
+                $totalItemPrice[] .= $value['product_price'] * $value['product_quantity'];
+            }
+            foreach($totalItemPrice as $price) {
+                $totalPrice += $price;
+            }
+            echo '<p style="color: #000;">To pay: £' . $totalPrice . '</p>';
+            ?>
+            <form action="/checkout-function.php" method="post">
+                <div class="checkout-container">
+                    <p class="checkout-container-text">Full Name: </p>
+                    <input type="text" name="username" class="checkout-container-input" required>
+                </div>
+                <div class="checkout-container">
+                    <p class="checkout-container-text">Email Address: </p>
+                    <input type="email" name="email" class="checkout-container-input" required>
+                </div>
+                <div class="checkout-container">
+                    <p class="checkout-container-text">Address: </p>
+                    <input type="text" name="address" class="checkout-container-input" required>
+                </div>
+                <div class="checkout-container">
+                    <p class="checkout-container-text">Postcode: </p>
+                    <input type="text" name="postcode" class="checkout-container-input" required>
+                </div>
+                <div class="checkout-container">
+                    <p class="checkout-container-text">City: </p>
+                    <input type="text" name="city" class="checkout-container-input" required>
+                </div>
+                <div class="checkout-container">
+                    <p class="checkout-container-text">Country: </p>
+                    <input type="text" name="country" class="checkout-container-input" required>
+                </div>
+                <div class="checkout-container">
+                    <p class="checkout-container-text">Phone Number: </p>
+                    <input type="text" name="phone" class="checkout-container-input" required>
+                </div>
+                <button id="checkout_button">Checkout</button>
+            </form>
+        <?php } else { ?>
+            <p style="color: #000;">You must be logged in to checkout</p>
+            <a href="log-in-page.php" style="color: #000;">Login</a>
+        <?php } ?>
     </div>
 </body>
 </html>
--- login-function.php
+++ login-function.php
@@ -1,45 +1,45 @@
-<?php 
+<?php
 session_start();
 
-$db = new mysqli("localhost", "u-210109870", "mVrSNyGnFNSJTDU", "u_210109870_db");   
+$db = new mysqli("localhost", "u-210109870", "mVrSNyGnFNSJTDU", "u_210109870_db");
 
-$email = stripcslashes($_GET['email']);  
-$password = stripcslashes($_GET['password']);  
+$email = stripcslashes($_GET['email']);
+$password = stripcslashes($_GET['password']);
 
-    $email = mysqli_real_escape_string($db, $email);  
-    $password = mysqli_real_escape_string($db, $password);  
-    
-    $sql = "SELECT name,email,password from `user`  where name = '$email' and password = '$password' ";  
-    $result = mysqli_query($db, $sql);  
-    // $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
-    $count = mysqli_num_rows($result);  
-   
-   
-      
-    if($count == 1){  
+    $email = mysqli_real_escape_string($db, $email);
+    $password = mysqli_real_escape_string($db, $password);
+
+    $sql = "SELECT name,email,password from `user`  where name = '$email' and password = '$password' ";
+    $result = mysqli_query($db, $sql);
+    // $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $count = mysqli_num_rows($result);
+
+
+
+    if($count === 1){
         $row = mysqli_fetch_assoc($result);
-       
+
         $_SESSION["username"] = $row["name"];
         $_SESSION["email"] =  $row["email"];
         $_SESSION["password"] =  $row["password"];
-        
-        $_SESSOPM["uservalid"] = "yes";
 
-       
+        $_SESSION["uservalid"] = "yes";
+
+
         include 'user-dashboard.php';
-      
-    }  
-    
-    else{  
-        echo "<script>alert('Login Unsuccessful')</script>";  
+
+    }
+
+    else{
+        echo "<script>alert('Login Unsuccessful')</script>";
 
         include 'log-in-page.php';
-    } 
-   
+    }
 
 
 
-?> 
+
+?>
 
 
 
