f3562d7dad80ccb8ad94aa114ca88d430093af06
Ellis Crowther <ellis.crowther@gambitnash.co.uk>
2022-12-01 10:15:47 +0000

    feature(theme): Maybe a cart page

--- cart.php
+++ cart.php
@@ -3,22 +3,25 @@ if (isset($_POST['product_price'])) {
 	session_start();
 	$product_price = $_POST['product_price'];
 	$product_id = $_POST['product_id'];
-	$product_quantity = $_POST['product_quantity'];
+	$product_quantity = $_POST['quantity'];
 	if(isset($_SESSION['cart'])) {
 		$cart = $_SESSION['cart'];
-		$cart[$product_id] = array("product_id" => $product_id, "product_price" => $product_price, "product_quantity" => $product_quantity);
+		if(isset($cart[$product_id])) {
+			echo '<p>Product already in cart</p>';
+		} else {
+			$cart[$product_id] = array("product_id" => $product_id, "product_price" => $product_price, "product_quantity" => $product_quantity);
+		}
 		$_SESSION['cart'] = $cart;
 	} else {
 		$cart = array();
 		$cart[$product_id] = array("product_id" => $product_id, "product_price" => $product_price, "product_quantity" => $product_quantity);
 		$_SESSION['cart'] = $cart;
 	}
-
-	print_r($_SESSION['cart']);
-
-//	foreach($_POST as $key=>$post_data){
-//		echo "You posted:" . $key . " = " . $post_data . "<br>";
-//	}
+	echo '<div class="cart-container" style="padding: 25px 0; width: 80%; margin: 0 auto;">';
+		foreach($_SESSION['cart'] as $key => $value) {
+			echo '<p>' . $value['product_id'] . ' ' . $value['product_price'] . ' ' . $value['product_quantity'] . '</p>>';
+		}
+	echo '</div>';
 } else {
 	echo '<h1>No product data, return to store</h1>';
 	echo '<a href="index.php">Return to store</a>';
