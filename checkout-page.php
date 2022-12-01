4d80b75eea6c24cb60610a06b7d663adbc05f2de
Ellis Crowther <ellis.crowther@gambitnash.co.uk>
2022-12-01 11:14:16 +0000

    chore(theme): Checkout form?

--- checkout-page.php
+++ checkout-page.php
@@ -10,8 +10,39 @@
         foreach($totalItemPrice as $price) {
             $totalPrice += $price;
         }
-        echo '<p style="color: #000;">To pay: ' . $totalPrice . '</p>';
+        echo '<p style="color: #000;">To pay: £' . $totalPrice . '</p>';
         ?>
+        <form action="./checkout-function.php" method="post">
+            <div class="checkout-container">
+                <p class="checkout-container-text">Full Name: </p>
+                <input type="text" name="username" class="checkout-container-input" required>
+            </div>
+            <div class="checkout-container">
+                <p class="checkout-container-text">Email Address: </p>
+                <input type="email" name="email" class="checkout-container-input" required>
+            </div>
+            <div class="checkout-container">
+                <p class="checkout-container-text">Address: </p>
+                <input type="text" name="address" class="checkout-container-input" required>
+            </div>
+            <div class="checkout-container">
+                <p class="checkout-container-text">Postcode: </p>
+                <input type="text" name="postcode" class="checkout-container-input" required>
+            </div>
+            <div class="checkout-container">
+                <p class="checkout-container-text">City: </p>
+                <input type="text" name="city" class="checkout-container-input" required>
+            </div>
+            <div class="checkout-container">
+                <p class="checkout-container-text">Country: </p>
+                <input type="text" name="country" class="checkout-container-input" required>
+            </div>
+            <div class="checkout-container">
+                <p class="checkout-container-text">Phone Number: </p>
+                <input type="text" name="phone" class="checkout-container-input" required>
+            </div>
+            <button id="checkout_button">Checkout</button>
+        </form>
     </div>
 </body>
 </html>
